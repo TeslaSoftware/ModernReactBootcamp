44c20479e50eba39ca79197c42ad2e63a7567f96
Sebastian Tysler <stysler@gmail.com>
2020-05-01 00:18:59 -0500

    added ability to roll dice

--- dice-roll/src/RollDice.js
+++ dice-roll/src/RollDice.js
@@ -2,18 +2,41 @@ import React, {Component} from 'react';
 import Die from './Die';
 
 class RollDice extends Component{
+    static defaultProps = {
+        sides: ["one", "two", "three", "four", "five", "six"]
+    };
+
     constructor(props){
         super(props);
         this.state = {
             die1: 'one',
-            die2: 'one'
+            die2: 'four'
         }
+        this.roll = this.roll.bind(this);
     }
+
+    getRandomSide = () => {
+        console.log(this.props);
+        return this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
+    }
+
+    roll(){
+        //pick 2 new rolls
+        const newDie1 = this.getRandomSide();
+        const newDie2 = this.getRandomSide();
+        //set state with new rolls
+        this.setState({
+            die1: newDie1,
+            die2: newDie2
+        });
+    }
+
     render(){
         return (
             <div>
                 <Die face={this.state.die1} />
-                <Die face={this.state.die1} />
+                <Die face={this.state.die2} />
+                <button onClick={this.roll}>Roll Dice</button>
             </div>
         )
     }
