11995d6b4c4293921833aaec507377f23d6916a9
Sebastian Tysler <stysler@gmail.com>
2020-05-28 17:44:30 -0500

    Added ability to edit Todo

--- todo-list/src/TodoList.js
+++ todo-list/src/TodoList.js
@@ -8,6 +8,7 @@ export default class TodoList extends Component {
         this.state = { todos: [] }
         this.create = this.create.bind(this);
         this.remove = this.remove.bind(this);
+        this.update = this.update.bind(this);
     }
 
     create(newTodo){
@@ -22,6 +23,17 @@ export default class TodoList extends Component {
         })
     }
 
+    update(id, updatedTask){
+        console.log('updating todo', id, updatedTask)
+        const updatedTodos = this.state.todos.map(todo => {
+            if(todo.id === id){
+                return {...todo, task: updatedTask}
+            }
+            return todo
+        });
+        this.setState({todos: updatedTodos})
+    }
+
     render(){
         const todos = this.state.todos.map(todo => {
             return( <Todo 
@@ -29,6 +41,7 @@ export default class TodoList extends Component {
                         key={todo.id} 
                         todoId={todo.id}
                         removeTodo={this.remove}
+                        updateTodo={this.update}
                     />);
         })
         return(
