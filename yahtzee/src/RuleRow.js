45fa60cad0d960328383011075f2f93a1c5a27a4
Sebastian Tysler <stysler@gmail.com>
2020-06-04 18:23:43 -0500

    Added description for each score option and its ability to display

--- yahtzee/src/RuleRow.js
+++ yahtzee/src/RuleRow.js
@@ -4,15 +4,15 @@ import './RuleRow.css'
 class RuleRow extends Component {
   
   render() {
-    const { score, name, doScore } = this.props;
+    const { score, name, doScore, description } = this.props;
     const isDisabled = score !== undefined;
     return (
       <tr 
-        className={`RuleRow RuleRow-${isDisabled ? "active" : "disabled"}`} 
+        className={`RuleRow RuleRow-${isDisabled ? "disabled" : "active"}`} 
         onClick={isDisabled ? null : doScore}
       >
         <td className="RuleRow-name">{name}</td>
-        <td className="RuleRow-score">{score}</td>
+        <td className="RuleRow-score">{isDisabled? score : description}</td>
       </tr>
     )
   }
