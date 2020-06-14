836cbc3022c4d9dda08055805912f2b592cc1a5e
Sebastian Tysler <stysler@gmail.com>
2020-06-13 21:08:58 -0500

    Added JokeList component and ability to fetch jokes

---
+++ dad-jokes/src/JokeList.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+export default class JokeList extends Component{
+    static defaultProps = { numJokesToGet: 10};
+
+    constructor(props){
+        super(props);
+        this.state ={ jokes: [] };
+    }
+
+    async componentDidMount(){
+        //Load jokes
+        let jokes = [];
+        while(jokes.length < this.props.numJokesToGet){ 
+            const res = await axios.get('https://icanhazdadjoke.com/', {
+                headers: {
+                    Accept: "application/json"
+                }
+            });
+            jokes.push(res.data.joke);
+        }
+        this.setState({ jokes })
+        console.log(jokes);
+    }
+
+    render(){
+        return(
+            <div className="JokeList">
+                <h1>Dad Jokes</h1>
+                <div className="JokeList-jokes">
+                    {this.state.jokes.map(j => {
+                        return (<div>{j}</div>)
+                    })}
+                </div>
+            </div>
+        )
+    }
+}
