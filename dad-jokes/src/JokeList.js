c04b12418f2e467db5db35c2f6ad892cd3469429
Sebastian Tysler <stysler@gmail.com>
2020-06-14 21:32:07 -0500

    Added sidebar and styled JokeList component

--- dad-jokes/src/JokeList.js
+++ dad-jokes/src/JokeList.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import './JokeList.css';
 
 export default class JokeList extends Component{
     static defaultProps = { numJokesToGet: 10};
@@ -27,7 +28,11 @@ export default class JokeList extends Component{
     render(){
         return(
             <div className="JokeList">
-                <h1>Dad Jokes</h1>
+                <div className="JokeList-sidebar">
+                    <h1 className="JokeList-title"><span>Dad</span> Jokes</h1>
+                    <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' alt="Laughing emoji" />
+                    <button className="JokeList-getmore">New Jokes</button>
+                </div>                
                 <div className="JokeList-jokes">
                     {this.state.jokes.map(j => {
                         return (<div>{j}</div>)
