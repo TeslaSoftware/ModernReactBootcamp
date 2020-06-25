d83ecba71d3d766cd02a5e02d2f8a59a4b4c52c1
Sebastian Tysler <stysler@gmail.com>
2020-06-25 15:12:32 -0500

    Added Message component, images and css files

--- vending-machine/src/Sardines.js
+++ vending-machine/src/Sardines.js
@@ -1,12 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import Message from "./Message";
+import "./Sardines.css";
 
-export default class Sardines extends Component {
-
-    render(){
-        return(
-            <div>
-                Sardines
-            </div>
-        )
-    }
+class Sardines extends Component {
+  render() {
+    return (
+      <div
+        className='Sardines'
+        style={{
+          backgroundImage:
+            "url(https://media.giphy.com/media/tVk4w6EZ7eGNq/giphy.gif)"
+        }}
+      >
+        <Message>
+          <h1>you don't eat the sardines. the sardines, they eat you!</h1>
+        </Message>
+      </div>
+    );
+  }
 }
+
+export default Sardines;
--- vending-machine/src/VendingMachine.js
+++ vending-machine/src/VendingMachine.js
@@ -1,12 +1,30 @@
 import React, {Component} from 'react';
+import Message from "./Message";
+import "./VendingMachine.css";
+import vendingMachineImg from "./VendingMachine.png";
 
 export default class VendingMachine extends Component {
-
-    render(){
-        return(
-            <div>
-                VendingMachine
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div
+        className='VendingMachine'
+        style={{ backgroundImage: `url(${vendingMachineImg})` }}
+      >
+        <Message>
+          <h1>hello i am a vending machine. what would you like to eat?</h1>
+        </Message>
+        <Message>
+          <h1>
+            
+          </h1>
+          <h1>
+            
+          </h1>
+          <h1>
+            
+          </h1>
+        </Message>
+      </div>
+    );
+  }
 }
