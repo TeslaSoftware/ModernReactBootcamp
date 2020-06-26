542b1158716b4cb2965d643682d62e4f12b01853
Sebastian Tysler <stysler@gmail.com>
2020-06-26 17:23:12 -0500

    Added Navbar component and its styling

--- vending-machine/src/App.js
+++ vending-machine/src/App.js
@@ -1,17 +1,21 @@
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import './App.css';
 import VendingMachine from './VendingMachine';
 import Chips from './Chips';
 import Sardines from './Sardines';
 import Soda from './Soda';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" render={() => <VendingMachine/>} />
-      <Route exact path="/soda" render={() => <Soda/>} />
-      <Route exact path="/sardines" render={() => <Sardines/>} />
-      <Route exact path="/chips" render={() => <Chips/>} />
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" render={() => <VendingMachine/>} />
+        <Route exact path="/soda" render={() => <Soda/>} />
+        <Route exact path="/sardines" render={() => <Sardines/>} />
+        <Route exact path="/chips" render={() => <Chips/>} />
+      </Switch>      
     </div>
   );
 }
