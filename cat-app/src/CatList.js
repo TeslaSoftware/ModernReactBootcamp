33784bb729f8aefd20331889310a5b2aa4c68ddf
Sebastian Tysler <stysler@gmail.com>
2020-07-05 23:11:36 -0500

    added styling

--- cat-app/src/CatList.js
+++ cat-app/src/CatList.js
@@ -6,14 +6,16 @@ class CatList extends Component {
     render() {
         return (
             <div className="CatList">
-                <h1 className="display1 text-center">Cat List!</h1>
+                <h1 className="display1 text-center my-4">Cat List!</h1>
                 <div className="row">
                     {this.props.cats.map(cat => (
                         <div className="Cat col-lg-4 text-center" key={cat.name}>
-                            <img src={cat.src} alt={`cat ${cat.name}`} />
-                            <h3>
-                                <Link className="underline" to={`/cats/${cat.name}`}>{cat.name}</Link>
-                            </h3>                            
+                            <Link to={`/cats/${cat.name}`}>
+                                <img src={cat.src} alt={`cat ${cat.name}`} />
+                                <h3 className="underline mt-3" >
+                                    {cat.name}
+                                </h3>   
+                            </Link>                         
                         </div>
                     ))}                       
                 </div>
