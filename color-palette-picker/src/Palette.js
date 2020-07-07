f7c572108b24f33001c71398c8605c1f6c9bef4e
Sebastian Tysler <stysler@gmail.com>
2020-07-06 21:43:35 -0500

    created react app and added Palette component and seed colors

---
+++ color-palette-picker/src/Palette.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Palette extends React.Component {
+    render() {
+        return(
+            <div className='Palette'>
+                {/*TODO: Navbar goes here */}
+                <div className="Palette-colors">
+                    {/*TODO: color boxes */}
+                </div>
+                {/*TODO: Footer*/}
+            </div>
+        )
+    }
+}
