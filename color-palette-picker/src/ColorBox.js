ea56a6c29a76ba6f76f3aa7b7e09ebc9a225c457
Sebastian Tysler <stysler@gmail.com>
2020-07-11 00:15:23 -0500

    Added animation for overlay to grow when color is copied

--- color-palette-picker/src/ColorBox.js
+++ color-palette-picker/src/ColorBox.js
@@ -3,11 +3,25 @@ import './ColorBox.css';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 export default class ColorBox extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { copied: false };
+        this.changeCopyState = this.changeCopyState.bind(this);
+    }
+
+    changeCopyState(){
+        this.setState({copied: true }, () => {
+            setTimeout(() => this.setState({ copied: false }), 1500);
+        })
+    }
+
     render() {
         const {name, background} = this.props;
         return(
-            <CopyToClipboard text={background} >
+            <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div style={{ background }} className="ColorBox">
+                    <div style={{ background }} className={`copy-overlay ${this.state.copied ? 'show' : ''}`}></div>
                     <div className="copy-container">
                         <div className="box-content">
                             <span>{name}</span>
