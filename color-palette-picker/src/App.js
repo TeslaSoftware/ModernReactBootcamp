6e0b8456c2330a83a2e7bf6e814cc02e03c7bfb2
Sebastian Tysler <stysler@gmail.com>
2020-07-31 23:15:37 -0500

    Add link to palette

--- color-palette-picker/src/App.js
+++ color-palette-picker/src/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
         <Route 
           exact 
           path="/"  
-          render={() => <PaletteList palettes={seedColors} />} 
+          render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />} 
         />
         <Route 
           exact 
